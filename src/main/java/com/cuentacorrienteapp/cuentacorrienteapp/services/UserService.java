2698769bd0577d1eb53d30a667386dd47cbb8489
MatiCastillo145 <maticastillo145@gmail.com>
2024-09-06 18:06:05 -0300

    Modifico la entidad User, además agrego su service y repository

---
+++ src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/services/UserService.java
@@ -0,0 +1,22 @@
+package com.cuentacorrienteapp.cuentacorrienteapp.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cuentacorrienteapp.cuentacorrienteapp.entities.User;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    User save (User user);
+
+    Optional<User> update (Long id, User user);
+
+    Optional<USer> delete (Long id);
+
+
+
+}
