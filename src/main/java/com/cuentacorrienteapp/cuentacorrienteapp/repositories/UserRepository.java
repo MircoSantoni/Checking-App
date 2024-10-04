d833f2a9c9c3f1c8c5e6beb2d9ec880fffcc8b94
MircoSantoni <santonimircoariel@gmail.com>
2024-10-04 17:38:56 -0300

    ,

--- src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/repositories/UserRepository.java
+++ src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/repositories/UserRepository.java
@@ -1,12 +1,5 @@
 package com.cuentacorrienteapp.cuentacorrienteapp.repositories;
 
-<<<<<<< Updated upstream
-import org.springframework.data.repository.CrudRepository;
-
-import com.cuentacorrienteapp.cuentacorrienteapp.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,5 +10,4 @@ import com.cuentacorrienteapp.cuentacorrienteapp.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
         Optional<User> findByEmail(String email);
->>>>>>> Stashed changes
 }
--- src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/validation/ValidationClass.java
+++ src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/validation/ValidationClass.java
@@ -1,14 +1,14 @@
-package com.cuentacorrienteapp.cuentacorrienteapp.validation;
+// package com.cuentacorrienteapp.cuentacorrienteapp.validation;
 
-import org.springframework.stereotype.Component;
-import org.springframework.validation.Validator;
+// import org.springframework.stereotype.Component;
+// import org.springframework.validation.Validator;
 
-@Component
-public class ValidationClass implements Validator {
+// @Component
+// public class ValidationClass implements Validator {
 
-    @Override
-    public boolean supports(Class<?> clazz){
-        return validable.class.isAssignableFrom(clazz);
-    }
+//     @Override
+//     public boolean supports(Class<?> clazz){
+//         return validable.class.isAssignableFrom(clazz);
+//     }
 
-}
+// }
