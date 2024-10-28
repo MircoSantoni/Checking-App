28061dd78a0fc563401cc65d4d5d79a87543957e
MircoSantoni <santonimircoariel@gmail.com>
2024-10-27 22:45:38 -0300

    warnings and new spring version

--- src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/dtos/user/ResponseLoginDto.java
+++ src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/dtos/user/ResponseLoginDto.java
@@ -2,7 +2,6 @@ package com.cuentacorrienteapp.cuentacorrienteapp.dtos.user;
 
 public record ResponseLoginDto(
     String id,
-    String cuentaId,
     String token,
     Long expiresIn
     ) {}
--- src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/services/implementation/MovimientoServiceImpl.java
+++ src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/services/implementation/MovimientoServiceImpl.java
@@ -1,11 +1,5 @@
 package com.cuentacorrienteapp.cuentacorrienteapp.services.implementation;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
