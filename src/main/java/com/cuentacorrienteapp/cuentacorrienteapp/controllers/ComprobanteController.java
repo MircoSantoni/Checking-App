9604c49e605f18899d7f46e20d56736ef0afbf54
MircoSantoni <santonimircoariel@gmail.com>
2024-10-30 21:08:08 -0300

    Comprobante

--- src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/controllers/ComprobanteController.java
+++ src/main/java/com/cuentacorrienteapp/cuentacorrienteapp/controllers/ComprobanteController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,40 +30,40 @@ public class ComprobanteController {
     @GetMapping("/mostrar-id/{id}")
     public ResponseEntity<ResponseComprobanteDto> viewId(@Valid @PathVariable String id) {
         ResponseComprobanteDto result = comprobanteService.findById(id);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // mostrar comprobantes segun numero de comprobante este debe de devolvver una lista pajin
     @GetMapping("/mostrar-comprobante/{nroComprobante}")
     public ResponseEntity<Set<ResponseComprobanteDto>> viewNroCompr(@Valid @PathVariable Long nroComprobante) {
         Set<ResponseComprobanteDto> result = comprobanteService.findByNroComprobante(nroComprobante);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     //mostrar comprobantes segun movimiento asignado
     @GetMapping("/mostrar")
     public ResponseEntity<Set<ResponseSetComprobanteDto>> viewComprobante() {
         Set<ResponseSetComprobanteDto> result = comprobanteService.findAll();
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // crear comprobante
     @PostMapping("/guardar")
     public ResponseEntity<ResponseComprobanteDto> saveComprobante(@Valid @RequestBody RequestComprobanteDto requestComprobanteDto) {
         ResponseComprobanteDto result = comprobanteService.saveComprobante(requestComprobanteDto);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
     
     @PostMapping("/cambiar-estado/{id}")
     public ResponseEntity<ResponseComprobanteDto> updateState(@Valid @PathVariable String id ) {
         ResponseComprobanteDto result = comprobanteService.updateIsValid(id);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
     
     // asignar un comprobante a un movimiento
     @PostMapping("/asignar-comprobante")
     public ResponseEntity<ResponseAsignComprobanteDto> setMovimiento(@Valid @RequestBody RequestAsignComprobanteDto requestAsignComprobanteDto) {
         ResponseAsignComprobanteDto result = comprobanteService.asignMovimiento(requestAsignComprobanteDto);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 }
